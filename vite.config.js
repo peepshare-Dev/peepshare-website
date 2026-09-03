d34ab5a1935754661ac5b777e73eecb86992d1d0
apecst <apec.sirinad.t@gmail.com>
2026-09-03 16:59:17 +0700

    ทำ page /join join with us

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home: resolve(__dirname, "index.html"),
+        homeAlias: resolve(__dirname, "home/index.html"),
+        homeCleanUrl: resolve(__dirname, "home.html"),
         partner: resolve(__dirname, "partner/index.html"),
         partnerShop: resolve(__dirname, "partner/shop/index.html"),
         blog: resolve(__dirname, "blog-tailwind/blog-tailwind.html"),
+        join: resolve(__dirname, "join/index.html"),
       },
     },
   },
